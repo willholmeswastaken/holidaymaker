237280f31a1379f83d6e32c639eec5858c8f153a
Will Holmes <willholmes97@icloud.com>
2023-04-19 21:35:02 +0100

    added skeleton ui to the scrapbook

--- src/pages/scrapbook.tsx
+++ src/pages/scrapbook.tsx
@@ -9,13 +9,12 @@ import GoogleMapsComponent from "@/components/google-maps-component";
 import { api } from "@/utils/api";
 import HolidaysList from "@/components/holidays-list";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const getServerSideProps = requireAuth(undefined, '/scrapbook');
 
 const Scrapbook: NextPage = () => {
     const { isLoading, data: holidays, isError } = api.holiday.getHolidays.useQuery();
-
-    if (isLoading) return <div>Loading...</div>;
 
     if (isError) return <div>Error</div>;
     return (
@@ -26,20 +25,31 @@ const Scrapbook: NextPage = () => {
                     <Button>Add Holiday</Button>
                 </Link>
             </div>
-            <Tabs defaultValue="account" className="w-full flex flex-col">
-                <TabsList className="w-[400px] self-center">
-                    <TabsTrigger value="account">Holiday Map</TabsTrigger>
-                    <TabsTrigger value="password">Holiday List</TabsTrigger>
-                </TabsList>
-                <TabsContent value="account">
-                    <GoogleMapsComponent position="center">
-                        <Map holidays={holidays} />
-                    </GoogleMapsComponent>
-                </TabsContent>
-                <TabsContent value="password">
-                    <HolidaysList holidays={holidays} />
-                </TabsContent>
-            </Tabs>
+            {
+                isLoading
+                    ? (
+                        <div className="flex flex-col space-y-4 mt-2">
+                            <Skeleton className="h-12 w-80 self-center" />
+                            <Skeleton className="h-80 w-full" />
+                        </div>
+                    )
+                    : (
+                        <Tabs defaultValue="account" className="w-full flex flex-col">
+                            <TabsList className="w-[400px] self-center">
+                                <TabsTrigger value="account">Holiday Map</TabsTrigger>
+                                <TabsTrigger value="password">Holiday List</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="account">
+                                <GoogleMapsComponent position="center">
+                                    <Map holidays={holidays} />
+                                </GoogleMapsComponent>
+                            </TabsContent>
+                            <TabsContent value="password">
+                                <HolidaysList holidays={holidays} />
+                            </TabsContent>
+                        </Tabs>
+                    )
+            }
         </>
     );
 };
