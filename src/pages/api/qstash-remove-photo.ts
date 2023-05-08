81a98c435e08221e8f3864ad4b8860975d27624c
Will Holmes <willholmes97@icloud.com>
2023-05-08 16:54:46 +0100

    Patched up qstash remove handler

--- src/pages/api/qstash-remove-photo.ts
+++ src/pages/api/qstash-remove-photo.ts
@@ -6,18 +6,25 @@ import { type HolidayPhoto } from "@prisma/client";
 import { verifySignature } from "@upstash/qstash/nextjs";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
+const s3 = new S3Client({ region: env.AWS_REGION });
+
+type RemovePhotoRequest = {
+  photo: HolidayPhoto;
+};
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   logger.info("QStash photo removal command called");
-  const photo = req.body as HolidayPhoto;
+  const { photo } = req.body as RemovePhotoRequest;
   const key = s3FilePathResolver(photo.photoFileName, photo.userId);
+  console.log(key);
   const command = new DeleteObjectCommand({ Bucket: env.AWS_BUCKET, Key: key });
   try {
-    const s3 = new S3Client({ region: env.AWS_REGION });
     await s3.send(command);
     logger.info("Photo removed from S3");
     res.status(200).end();
   } catch (error) {
     logger.error("Error removing photo from S3", error);
+    console.error(error);
     res.status(500).end();
   }
 }
